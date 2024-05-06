1bd449109c2e8d09b00d3209b0a89fd37b22ed8f
RichardKobay <ricardo.roel110@gmail.com>
2024-05-06 07:31:30 -0600

    Added BrScanner and FileUtils

--- dataBaseManager/src/main/java/edu/upvictoria/fpoo/utils/BrScanner.java
+++ dataBaseManager/src/main/java/edu/upvictoria/fpoo/utils/BrScanner.java
@@ -1,4 +1,65 @@
 package edu.upvictoria.fpoo.utils;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * This class will help with a user inputs
+ * */
 public class BrScanner {
+    /**
+     * Will read a String line and return to the user
+     * @return String: The user input
+     * */
+    public static String readLine() {
+        String line = "";
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            line = br.readLine();
+        } catch (IOException e) {
+            System.err.println("There was an error reading the input");
+        }
+
+        return line;
+    }
+
+    /**
+     * Will read an integer from a user input
+     * If the user enters a non integer input, will be showed by the console output
+     * @return int: The integer user input
+     * */
+    public static int readInt() {
+        int line = 0;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            line = Integer.parseInt(br.readLine());
+        } catch (IOException e) {
+            System.err.println("There was an error reading the input");
+        } catch (NumberFormatException e) {
+            System.err.println("La entrada ingresada no es un número");
+        }
+
+        return line;
+    }
+
+    /**
+     * Will read a double from a user input
+     * If the user enters a non-double input, will be showed by the console output
+     * @return double: The double user input
+     * */
+    public static double readDouble() {
+        double line = 0;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        try {
+            line = Double.parseDouble(br.readLine());
+        } catch (IOException e) {
+            System.err.println("There was an error reading the input");
+        } catch (NumberFormatException e) {
+            System.err.println("La entrada ingresada no es un número");
+        }
+        return line;
+    }
+
 }
--- dataBaseManager/src/main/java/edu/upvictoria/fpoo/utils/FileUtils.java
+++ dataBaseManager/src/main/java/edu/upvictoria/fpoo/utils/FileUtils.java
@@ -1,4 +1,26 @@
 package edu.upvictoria.fpoo.utils;
 
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
 public class FileUtils {
+    protected InputStream getFileFromResourceAsStream(String fileName) {
+        ClassLoader classLoader = getClass().getClassLoader();
+        InputStream inputStream = classLoader.getResourceAsStream(fileName);
+
+        if (inputStream == null) {
+            throw new IllegalArgumentException("file not found! " + fileName);
+        } else {
+            return inputStream;
+        }
+    }
+
+    protected BufferedReader fromInputStreamToBufferedReader (InputStream is) {
+        return new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
+    }
+
+    public InputStream getFileAsInputStream(String fileName) {
+        FileUtils fileUtils = new FileUtils();
+        return fileUtils.getFileFromResourceAsStream(fileName);
+    }
 }
